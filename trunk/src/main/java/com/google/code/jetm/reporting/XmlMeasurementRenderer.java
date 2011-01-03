1d570d2f12fe7997330a516c73199d1be426b1a1
jrh3k5 <jrh3k5@45b34600-6f9b-fe22-ba5e-d9036baf958e>
2011-01-03 00:11:39 +0000

    Initial check-in of code

---
+++ trunk/src/main/java/com/google/code/jetm/reporting/XmlMeasurementRenderer.java
@@ -0,0 +1,16 @@
+package com.google.code.jetm.reporting;
+
+import java.util.Map;
+
+import etm.core.renderer.MeasurementRenderer;
+
+public class XmlMeasurementRenderer implements MeasurementRenderer {
+    /**
+     * {@inheritDoc}
+     */
+    public void render(Map points) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ trunk/src/test/java/com/google/code/jetm/reporting/xml/AggregateBinderTest.java
@@ -0,0 +1,57 @@
+package com.google.code.jetm.reporting.xml;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import etm.core.aggregation.Aggregate;
+
+/**
+ * Unit tests for {@link AggregateBinder}.
+ * 
+ * @author jrh3k5
+ * 
+ */
+
+public class AggregateBinderTest {
+    private final AggregateBinder binder = new AggregateBinder();
+
+    /**
+     * Test the marshalling of an aggregate to XML.
+     * 
+     * @throws Exception
+     *             If any errors occur during the test run.
+     */
+    @Test
+    public void testTransform() throws Exception {
+        final double min = 1.0;
+        final double max = 2.0;
+        final double total = 4.0;
+        final long measurements = 1500;
+        final String name = "measurement-name";
+
+        final XmlAggregate aggregate = mock(XmlAggregate.class);
+        when(aggregate.getMax()).thenReturn(max);
+        when(aggregate.getMin()).thenReturn(min);
+        when(aggregate.getTotal()).thenReturn(total);
+        when(aggregate.getMeasurements()).thenReturn(measurements);
+        when(aggregate.getName()).thenReturn(name);
+
+        final String xml = binder.marshall(Collections.singletonList(aggregate));
+
+        // Reconstitute it as a bean
+        final List<Aggregate> unmarshalledList = binder.unmarshall(xml);
+        assertThat(unmarshalledList).hasSize(1);
+        final Aggregate unmarshalled = unmarshalledList.get(0);
+        assertThat(unmarshalled.getMax()).isEqualTo(max);
+        assertThat(unmarshalled.getMin()).isEqualTo(min);
+        assertThat(unmarshalled.getTotal()).isEqualTo(total);
+        assertThat(unmarshalled.getMeasurements()).isEqualTo(measurements);
+        assertThat(unmarshalled.getName()).isEqualTo(name);
+    }
+}
